809e5ac8311e9530add63cd094626231912668ab
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-17 12:06:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 function stringChop(str, size) {
   // your code here
+	let arr=[];
+function stringChop(str, size) {
+    let arr = [];
+    for (let i = 0; i < str.length; i += size) {
+        let chunk = str.substring(i, i + size);
+        arr.push(chunk);
+    }
+    return arr;
 }
 
 // Do not change the code below
-const str = prompt("Enter String.");
-const size = prompt("Enter Chunk Size.");
+const str = propt("Enter String.");
+const size = propt("Enter Chunk Size.");
 alert(stringChop(str, size));
