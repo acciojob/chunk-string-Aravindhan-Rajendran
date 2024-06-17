4958ed2166a88ce07736db328dcf0556c2c20a3c
Aravindhan-Rajendran <103531993+Aravindhan-Rajendran@users.noreply.github.com>
2024-06-17 12:18:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,11 +1,16 @@
 
 function stringChop(str, size) {
+	    if (str === null) {
+        return [];
+    }
+	
     let arr = [];
     for (let i = 0; i < str.length; i += size) {
         let chunk = str.substring(i, i + size);
         arr.push(chunk);
     }
     return arr;
+
 }
 
 // Do not change the code below
